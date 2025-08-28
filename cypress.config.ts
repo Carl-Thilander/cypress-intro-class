86d47aafadaddcc8b89245c45fc1b99112738c3d
David Jensen <tsourdox@icloud.com>
2025-08-28 10:35:36 +0200

    describe necessary steps to setup for our test environment

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,8 +5,13 @@ import { seedTodos } from "./prisma/seed/todo";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
       on("task", {
+        // 1. Skapa en in-memory databas (replica set prisma gnäller annars)
+        // 2. Starta Next.js servern (på en annan port som ansluter till 1.)
+        // 3. Vänta på att Next.js servern är igång innan cypress kör vidare
+        // 4. Städa upp processerna dvs Mongo databasen och Next.js servern
+        // 5. Reseeda om databasen så att testerna blir oberoende av varandra
+
         async reseed() {
           await db.todo.deleteMany();
           await seedTodos();
